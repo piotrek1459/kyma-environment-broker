7a232fe3ac13462b7f2875d8acdcc6c19e11f06c
Piotr Miśkiewicz <piotr.miskiewicz@sap.com>
2025-11-17 10:27:13 +0100

    Fix notable changes validation (#2520)
    * Fix notable changes validation
    
    * wip
    
    * wip

--- scripts/notable_changes_validate.sh
+++ scripts/notable_changes_validate.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
 # This script validates that if there are any files (other than README.md) in the notable-changes-to-release.
-# If yes, we require that notable-changes-to-release/notable-changes.md exists.
+# If yes, we require that notable-changes-to-release/notable-change.md exists.
 
 files=$(find notable-changes-to-release -type f -not -name 'README.md')
 if [ -n "$files" ]; then
-    # Check if notable-changes.md exists
-    if [ ! -f notable-changes-to-release/notable-changes.md ]; then
-        echo "Validation failed: notable-changes-to-release/notable-changes.md is missing."
+    # Check if notable-change.md exists
+    if [ ! -f notable-changes-to-release/notable-change.md ]; then
+        echo "Validation failed: notable-changes-to-release/notable-change.md is missing."
         exit 1
     fi
 fi
