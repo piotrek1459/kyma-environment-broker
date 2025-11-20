3895809f04d882780d6e9a679da53947e92f5ad1
Krupinski <piotr.krupinski@sap.com>
2025-11-20 15:40:36 +0100

    Fix merge

--- internal/broker/schemas.go
+++ internal/broker/schemas.go
@@ -130,16 +130,6 @@ func (s *SchemaService) defaultServicePlan(id, name string, plans PlansConfig, c
 	return servicePlan
 }
 
-
-func (s *SchemaService) createUpdateSchemas(machineTypesDisplay, additionalMachineTypesDisplay, regionsDisplay map[string]string, machineTypes, additionalMachineTypes, regions []string, flags ControlFlagsObject) (create, update *map[string]interface{}) {
-	createProperties := NewProvisioningProperties(machineTypesDisplay, additionalMachineTypesDisplay, regionsDisplay, machineTypes, additionalMachineTypes, regions, false, flags.rejectUnsupportedParameters, s.configProvider, s.runtimeConfigMapName)
-	updateProperties := NewProvisioningProperties(machineTypesDisplay, additionalMachineTypesDisplay, regionsDisplay, machineTypes, additionalMachineTypes, regions, true, flags.rejectUnsupportedParameters, s.configProvider, s.runtimeConfigMapName)
-
-	return createSchemaWithProperties(createProperties, s.defaultOIDCConfig, false, requiredSchemaProperties(), flags),
-		createSchemaWithProperties(updateProperties, s.defaultOIDCConfig, true, requiredSchemaProperties(), flags)
-}
-
-
 func (s *SchemaService) planSchemas(cp pkg.CloudProvider, planName, platformRegion string) (create, update *map[string]interface{}, available bool) {
 	regions := s.planSpec.Regions(planName, platformRegion)
 	if len(regions) == 0 {
@@ -161,11 +151,11 @@ func (s *SchemaService) planSchemas(cp pkg.CloudProvider, planName, platformRegi
 		regions,
 		false,
 		flags.rejectUnsupportedParameters,
-		s.configProvider,
-		s.runtimeConfigMapName,
 		s.providerSpec,
 		cp,
 		s.cfg.DualStackDocsURL,
+		s.configProvider,
+		s.runtimeConfigMapName,
 	)
 	updateProperties := NewProvisioningProperties(
 		s.providerSpec.MachineDisplayNames(cp, machines),
@@ -176,11 +166,11 @@ func (s *SchemaService) planSchemas(cp pkg.CloudProvider, planName, platformRegi
 		regions,
 		true,
 		flags.rejectUnsupportedParameters,
-		s.configProvider,
-		s.runtimeConfigMapName,
 		s.providerSpec,
 		cp,
 		s.cfg.DualStackDocsURL,
+		s.configProvider,
+		s.runtimeConfigMapName,
 	)
 	return createSchemaWithProperties(createProperties, s.defaultOIDCConfig, false, requiredSchemaProperties(), flags),
 		createSchemaWithProperties(updateProperties, s.defaultOIDCConfig, true, requiredSchemaProperties(), flags), true
@@ -236,11 +226,11 @@ func (s *SchemaService) AzureLiteSchema(platformRegion string, regions []string,
 		regions,
 		update,
 		flags.rejectUnsupportedParameters,
-		s.configProvider,
-		s.runtimeConfigMapName,
 		s.providerSpec,
 		pkg.Azure,
 		s.cfg.DualStackDocsURL,
+		s.configProvider,
+		s.runtimeConfigMapName,
 	)
 	properties.AutoScalerMax.Minimum = 2
 	properties.AutoScalerMax.Maximum = 40
