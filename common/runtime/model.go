02e1017dda0818c9e49b091e2d179e92d17ad501
Krupinski <piotr.krupinski@sap.com>
2025-12-16 11:40:36 +0100

    Change for kcp-cli

--- common/runtime/model.go
+++ common/runtime/model.go
@@ -539,7 +539,7 @@ type Channel *string
 type ModulesDTO struct {
 	Channel Channel     `json:"channel,omitempty" yaml:"channel,omitempty"`
 	Default *bool       `json:"default,omitempty" yaml:"default,omitempty"`
-	List    []ModuleDTO `json:"list" yaml:"list"`
+	List    []ModuleDTO `json:"list,omitempty" yaml:"list,omitempty"`
 }
 
 type CustomResourcePolicy *string
